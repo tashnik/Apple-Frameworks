1278ea1106f4405a53e7bc212573a122349bf439
David Potashnik <davidpotashnik@yahoo.com>
2020-11-28 13:16:32 -0500

    first commit

---
+++ Apple-Frameworks/Screens/FrameworkDetailView/FrameworkDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FrameworkDetailViewModel.swift
+//  Apple-Frameworks
+//
+//  Created by Tashnik on 11/28/20.
+//
+
+import Foundation
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Apple-Frameworks
+# Apple-Frameworks
